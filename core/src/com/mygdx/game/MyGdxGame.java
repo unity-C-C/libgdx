1e848eba2c576cab29ccee28a4d1ac0dea2b0a7d
ZHAOHAN HE <johnhezhaohan@aol.com>
2020-06-11 19:23:53 +0800

    Game 2

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -30,27 +30,50 @@ public class MyGdxGame extends ApplicationAdapter {
     int hp=5;
 
 
-    class zhadan{
-        Texture zhadanimg;
-        int id=0;
-
-        zhadan(){
+    class enemy{
+        Texture enemyimg;
+        int id;
+        float x;
+        float y;
+        float speed;
+        enemy(){
             super();
-            zhadanimg=new Texture("play.png");
+        }
+
+        enemy(int id,Texture texture){
+            super();
+            this.id=id;
+            this.enemyimg=texture;
+        }
+
+        float getSpeed(){
+            return this.speed;
+        }
+
+        void setSpeed(float speed){
+            this.speed=speed;
+        }
+
+        float getX(){
+            return this.x;
+        }
+
+        float getY(){
+            return this.y;
+        }
+
+        void setX(float x){
+            this.x=x;
+        }
+
+        void setY(float y){
+            this.y=y;
         }
     }
 
-    class jinpi {
-        Texture jinpiimg;
-        int id=1;
 
-        jinpi(){
-            super();
-            jinpiimg=new Texture("play.png");
-        }
-    }
 
-    ArrayList zhadanlis=new ArrayList();
+    ArrayList<enemy> enemylis=new ArrayList<enemy>();
 
 
 	@Override
@@ -109,11 +132,44 @@ public class MyGdxGame extends ApplicationAdapter {
                 //Draw player, player score, player HP
                 //Randomly draw the coins and bombs falling from the sky to get the gold score +1 to get the bomb HP-1 Player HP<=0 The end of the game The more difficult the game is, the greater the difficulty will be. The game will record the score
 
-                font.draw(batch,"return",0,y-20);
+                font.draw(batch,"return",0,y);
+
+                //显示玩家信息
+                font.draw(batch,"HP:"+hp,x-50,y);
+                font.draw(batch,"SCORE:"+score,x-80,y-20);
+
 
                 //随机绘制炸弹、金币
-                batch.draw(playimg,playx,0);
+                batch.draw(playimg,playx,0);//绘制玩家
 
+                int N=(int)(Math.random()*100);
+
+                if(N-score>0){
+                    int dw=(int)(Math.random()*100);
+                    if(dw>97){//绘制金币
+                        enemy jinp = new enemy(0,new Texture("bomb.png"));
+                        float xx=(float)(Math.random()*x/2+100);
+                        batch.draw(jinp.enemyimg,xx,y);
+                        jinp.setSpeed((float)Math.random()*5);
+                        jinp.setX(xx);
+                        jinp.setY(y);
+                        jinp.id=0;
+                        enemylis.add(jinp);
+                    }else if(dw<1){
+                        enemy zhada=new enemy(1,new Texture("gold.png"));
+                        float xx = (float) (Math.random()*x/2+100);
+                        batch.draw(zhada.enemyimg,xx,y);
+                        zhada.setY(y);
+                        zhada.setX(xx);
+                        zhada.setSpeed((float)Math.random()*5);
+                        zhada.id=1;
+                        enemylis.add(zhada);
+                    }
+                }
+
+                move();
+                collision();
+                victory();
 
                 int mx=Gdx.input.getX();
                 int my=Gdx.input.getY();
@@ -122,11 +178,17 @@ public class MyGdxGame extends ApplicationAdapter {
                 boolean pan = Gdx.input.isButtonPressed(Input.Buttons.LEFT);
 
                 if(pan){
-                    if(mx>0&&mx<40
-                            &&my>y-20&&my<y){
+                    if(mx>0&&mx<100
+                            &&my>y-50&&my<y){
+
                         scene=0;
+                        hp=5;
+                        enemylis=new ArrayList<enemy>();
+                        playx=x/2-50;
+                        scoreed=score;
                     }else{
-                        if((mx+71)<=x){
+                        if((mx+60)<=x&&
+                        mx+10>0){
                             playx=mx;
                         }
                     }
@@ -141,15 +203,15 @@ public class MyGdxGame extends ApplicationAdapter {
                 font.draw(batch,"results:"+scoreed,x/2-50,
                         y/2+20);
 
-                font.draw(batch,"return",0,y-20);
+                font.draw(batch,"return",0,y);
 
                 boolean pan = Gdx.input.isButtonPressed(Input.Buttons.LEFT);
                 int mx=Gdx.input.getX();
                 int my=Gdx.input.getY();
                 my=y-my;
                 if(pan){
-                    if(mx>0&&mx<40
-                    &&my>y-20&&my<y){
+                    if(mx>0&&mx<100
+                    &&my>y-50&&my<y){
                         scene=0;
                     }
                 }
@@ -166,4 +228,61 @@ public class MyGdxGame extends ApplicationAdapter {
 		img1.dispose();
 		font.dispose();
 	}
+
+	//碰撞检测
+    void collision(){
+	    for(int i=0;i<enemylis.size();++i){
+	        if(enemylis.get(i).id==0){
+                if(rect(i)){
+                    hp--;
+                    enemylis.remove(i);
+                }
+            }else if(enemylis.get(i).id==1){
+                if(rect(i)){
+                    score++;
+                    enemylis.remove(i);
+                }
+            }
+        }
+    }
+
+    boolean rect(int i){
+	    if((playx+10)>=enemylis.get(i).x&&(playx+10)<=enemylis.get(i).x+53
+        &&48>=enemylis.get(i).y&&48<=enemylis.get(i).y+48){
+	        return true;
+        }else if((playx+60)>=enemylis.get(i).x&&(playx+60)<=enemylis.get(i).x+53
+        &&48>=enemylis.get(i).y&&48<=enemylis.get(i).y+48){
+	        return true;
+        }
+
+	    return false;
+    }
+
+    //胜负判断
+    boolean victory(){
+	    if(hp<=0){
+	        scene=0;
+	        hp=5;
+	        enemylis=new ArrayList<enemy>();
+	        playx=x/2-50;
+	        scoreed=score;
+
+	        return true;
+        }
+	    return false;
+    }
+
+    //金币、炸弹向下运动
+    void move(){
+       for(int i=0;i<enemylis.size();++i){
+           enemylis.get(i).y-=enemylis.get(i).getSpeed();
+           batch.draw(enemylis.get(i).enemyimg,enemylis.get(i).x,enemylis.get(i).y);
+           if(enemylis.get(i).getY()<0){
+              if(enemylis.get(i).id==1){
+                  hp-=1;
+              }
+               enemylis.remove(i);
+           }
+       }
+    }
 }
